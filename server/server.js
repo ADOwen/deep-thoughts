cfc9f7db1efb54ef76a305ca803b2fee3ecd1832
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-11-06 14:53:20 -0500

    connected front-end application to display GraphQL thought query on home page and added ability run client and server from root directory concurrently

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -34,6 +35,15 @@ db.once('open', () => {
     })
   })
 };
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
