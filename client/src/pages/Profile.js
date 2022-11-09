8021cfe5207066aad73bc4949882ddf208f68ef8
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-11-09 14:59:11 -0500

    created thought form component and included it to display on the home and profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendsList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -13,7 +15,19 @@ const Profile = () => {
     variables: { username: userParam }
   });
 
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   const user = data?.me || data?.user || {};
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/profile" />;
@@ -31,6 +45,8 @@ const Profile = () => {
     );
   }
 
+
+
   return (
     
     <div>
@@ -38,6 +54,11 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -53,6 +74,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
